da94de994307f9057043a8c2fdd369846843b041
Nixiak <114225150+NixiakAI@users.noreply.github.com>
2026-04-01 11:49:46 +0800

    Update jetbrains_servers_updater.py

--- jetbrains_servers_updater.py
+++ jetbrains_servers_updater.py
@@ -31,6 +31,7 @@ def Shodanapi():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
     }
+    matches = []
 
     try:
         session = requests.Session()
